2a7863742c22eff670eed853dd69e67733b60515
Zack <schaxmann@gmail.com>
2023-04-21 12:02:56 +0100

    Sizing adjustments for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,6 +39,13 @@ export const TicketDiv = styled.div<DivProps>`
   /* :hover {
     transform: rotateY(180deg);
   } */
+
+  @media (max-width: 500px) {
+    height: ${(dimensions) =>
+      dimensions.height && `${dimensions.height * 0.8}px`};
+    width: ${(dimensions) =>
+      dimensions.height && `${dimensions.height * 0.8 * 0.48473282441}px`};
+  }
 `;
 
 export const TicketImage = styled.img<Props>`
